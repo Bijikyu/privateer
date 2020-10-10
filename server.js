905401d509e31954341cb87a8ef63647a29a08e9
Brian Quezada <bgquezada@gmail.com>
2020-10-10 14:48:47 -0700

    require dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+require('dotenv').config();
 
 require('./config/database');
 
