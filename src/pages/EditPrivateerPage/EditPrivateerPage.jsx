6bed44ac8e5b9a4047039438331fad46f2d964a7
Momo <cocomojomomo@protonmail.com>
2024-04-17 21:10:38 +0000

    Add code comments

--- src/pages/EditPrivateerPage/EditPrivateerPage.jsx
+++ src/pages/EditPrivateerPage/EditPrivateerPage.jsx
@@ -1,60 +1,65 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import ExplainerBooty from '../../components/ExplainerItems/ExplainerBooty.jsx'
-import ExplainerBounty from '../../components/ExplainerItems/ExplainerBounty.jsx'
-import ExplainerCanon from '../../components/ExplainerItems/ExplainerCanon.jsx'
+// This is a React component for editing a "privateer" entity. It includes form handling and uses React Router for navigation.
+import React, {Component} from 'react'; // Importing React and Component from the react package
+import {Link} from 'react-router-dom'; // Importing Link from react-router-dom for navigation without page refresh
+import ExplainerBooty from '../../components/ExplainerItems/ExplainerBooty.jsx' // Importing a custom component to explain the "booty" field
+import ExplainerBounty from '../../components/ExplainerItems/ExplainerBounty.jsx' // Importing a custom component to explain the "bounty" field
+import ExplainerCanon from '../../components/ExplainerItems/ExplainerCanon.jsx' // Importing a custom component to explain the "canon" field
 
+// The EditPrivateerPage component allows users to edit details of a privateer entity.
 class EditPrivateerPage extends Component {
   state = {
-    invalidForm: false,
-    formData: this.props.location.state.privateer
+    invalidForm: false, // State to track if the form is valid or not
+    formData: this.props.location.state.privateer // State to hold the form data passed via React Router
   };
 
-  formRef = React.createRef();
+  formRef = React.createRef(); // Creating a ref for the form to check its validity
 
+  // Handles form submission, prevents default form submission and calls a function to update the privateer
   handleSubmit = e => {
-    e.preventDefault();
-    this.props.handleUpdatePrivateer(this.state.formData);
+    e.preventDefault(); // Prevents the default form submission action
+    this.props.handleUpdatePrivateer(this.state.formData); // Calls the function to handle updating the privateer with the current form data
   };
 
+  // Handles changes in the form inputs and updates the state accordingly
   handleChange = e => {
-    const formData = {...this.state.formData, [e.target.name]: e.target.value};
+    const formData = {...this.state.formData, [e.target.name]: e.target.value}; // Creates a new formData object with the updated field
     this.setState({
-      formData,
-      invalidForm: !this.formRef.current.checkValidity()
+      formData, // Sets the new formData to state
+      invalidForm: !this.formRef.current.checkValidity() // Updates the invalidForm state based on form validity
     });
   };
 
+  // Renders the component's JSX
   render() {
     return (
       <>
         <br/>
-        <h1>Edit Privateer</h1>
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="row frow">
-            <div className="group">
-              <ExplainerBooty/>
-              <label>Product or Brand Name: </label>
-              <input className="form-control" name="booty" required
-                value={this.state.formData.booty} onChange={this.handleChange}/>
+        <h1>Edit Privateer</h1> // Heading for the page
+        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}> // The form element with a reference and onSubmit handler
+          <div className="row frow"> // Container for form groups
+            <div className="group"> // Group for the "booty" input field
+              <ExplainerBooty/> // Component to explain the "booty" field
+              <label>Product or Brand Name: </label> // Label for the "booty" input field
+              <input className="form-control" name="booty" required // Input field for "booty" with validation
+                value={this.state.formData.booty} onChange={this.handleChange}/> // Binding the input value and change handler
             </div>
-            <div className="group">
-              <ExplainerBounty/>
-              <label>Bounty: $</label>
-              <input className="form-control" name="bounty" required
-                value={this.state.formData.bounty} onChange={this.handleChange}/>
+            <div className="group"> // Group for the "bounty" input field
+              <ExplainerBounty/> // Component to explain the "bounty" field
+              <label>Bounty: $</label> // Label for the "bounty" input field
+              <input className="form-control" name="bounty" required // Input field for "bounty" with validation
+                value={this.state.formData.bounty} onChange={this.handleChange}/> // Binding the input value and change handler
             </div>
-            <div className="group">
-              <ExplainerCanon/>
-              <label>Your Cease and Desist</label>
-              <textarea className="form-control" name="canon"
-                value={this.state.formData.canon} onChange={this.handleChange}/>
+            <div className="group"> // Group for the "canon" textarea field
+              <ExplainerCanon/> // Component to explain the "canon" field
+              <label>Your Cease and Desist</label> // Label for the "canon" textarea field
+              <textarea className="form-control" name="canon" // Textarea field for "canon"
+                value={this.state.formData.canon} onChange={this.handleChange}/> // Binding the textarea value and change handler
             </div>
           </div>
-          <button type="submit" className="btn btn-xs btn-danger" disabled={this.state.invalidForm}>RENEW YOUR PRIVATEER!</button>
+          <button type="submit" className="btn btn-xs btn-danger" disabled={this.state.invalidForm}>RENEW YOUR PRIVATEER!</button> // Submit button, disabled if form is invalid
           <br></br>
           <br></br>
-          <Link to='/'>CANCEL</Link>
+          <Link to='/'>CANCEL</Link> // Link to navigate back to the home page without submitting the form
         </form>
         <br></br>
       </>
@@ -62,4 +67,4 @@ class EditPrivateerPage extends Component {
   }
 }
 
-export default EditPrivateerPage;
+export default EditPrivateerPage; // Exporting the EditPrivateerPage component for use in other parts of the application
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import PrivateerList from "../../components/PrivateerList/PrivateerList.jsx"
-import Explainer from "../../components/Explainer/Explainer.jsx"
+// This is a React component file that defines the MainPage component. It imports necessary modules and components, sets up the MainPage functional component, and exports it for use in other parts of the application.
 
+import React from 'react'; // Importing the React library to enable JSX syntax and component logic
+import {Link} from 'react-router-dom'; // Importing Link component from react-router-dom for client-side navigation
+import PrivateerList from "../../components/PrivateerList/PrivateerList.jsx" // Importing PrivateerList component from the specified path
+import Explainer from "../../components/Explainer/Explainer.jsx" // Importing Explainer component from the specified path
 
+// Defines the MainPage functional component that renders the main content of the page
 const MainPage = (props) => {
   return (
-    <div>
-      <br></br>
-      <PrivateerList 
-        user={props.user}
-        privateers={props.privateers}
-        handleDeletePrivateer={props.handleDeletePrivateer}
+    <div> // Start of the main container div
+      <br></br> // Line break for spacing
+      <PrivateerList // PrivateerList component that takes in user, privateers, and handleDeletePrivateer props
+        user={props.user} // Prop that passes the user data to the PrivateerList component
+        privateers={props.privateers} // Prop that passes the list of privateers to the PrivateerList component
+        handleDeletePrivateer={props.handleDeletePrivateer} // Prop that passes the delete handler function to the PrivateerList component
       />
-      <Explainer/>
-      <h3>Protect your Brand or Product from Piracy and Exploitation</h3>
-      <p className="text">This service enables businesses and creators to 
+      <Explainer/> // Explainer component that provides additional information or context
+      <h3>Protect your Brand or Product from Piracy and Exploitation</h3> // Heading for the section
+      <p className="text">This service enables businesses and creators to // Paragraph with class "text" describing the service
         automatically have the web monitored for their products and 
         brands in order to protect them from piracy and exploitation. 
         When an offender is found, a cease and desist letter is sent 
         out to the webmaster of the offending site.
-        <br></br> 
-        <br></br> 
+        <br></br> // Line break for spacing
+        <br></br> // Line break for spacing
         Once you are logged in, "Commission a Privateer" will appear 
         in the navigation bar. Just select that to choose the products and 
         brands you want to protect, to set a bounty for each succesful takedown, 
@@ -29,15 +31,12 @@ const MainPage = (props) => {
         privateer is "commissioned" you will see them in the Fleet 
         section of the website. We are now monitoring the high seas (the web) 
         in pursuit of your wayward contraband.</p>
-        <br></br> 
-        <br></br> 
-        <br></br> 
-        <br></br> 
-    </div>
+        <br></br> // Line break for spacing
+        <br></br> // Line break for spacing
+        <br></br> // Line break for spacing
+        <br></br> // Line break for spacing
+    </div> // End of the main container div
   );
 };
 
-export default MainPage;
-
-
-
+export default MainPage; // Exports the MainPage component for use in other parts of the application
